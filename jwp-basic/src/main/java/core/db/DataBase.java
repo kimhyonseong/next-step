f6ea088abb860d83d108138566a94b8771f8e330
kimhyonseong <corea6524@gmail.com>
2023-02-19 23:29:42 +0900

    feat : user info modify

--- jwp-basic/src/main/java/core/db/DataBase.java
+++ jwp-basic/src/main/java/core/db/DataBase.java
@@ -6,8 +6,11 @@ import java.util.Map;
 import com.google.common.collect.Maps;
 
 import next.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DataBase {
+    private static final Logger log = LoggerFactory.getLogger(DataBase.class);
     private static Map<String, User> users = Maps.newHashMap();
 
     public static void addUser(User user) {
@@ -16,6 +19,18 @@ public class DataBase {
 
     public static User findUserById(String userId) {
         return users.get(userId);
+    }
+
+    public static void modifyUser(User user) {
+        User beforeUser = users.get(user.getUserId());
+        log.info("param : {}",user);
+        log.info("before : {}",beforeUser);
+
+        if (beforeUser != null) {
+            user = new User(user.getUserId(),user.getPassword(),user.getName(),user.getEmail());
+            users.put(user.getUserId(), user);
+            log.info("put : {}",users.get(user.getUserId()));
+        }
     }
 
     public static Collection<User> findAll() {
